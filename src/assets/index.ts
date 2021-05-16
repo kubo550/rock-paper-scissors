2b0e88666edda97b1f6b41f1aec3589bfa55b916
powerty <qwercy142@gmail.com>
2021-05-16 21:08:53 +0200

    Initial App

---
+++ src/assets/index.ts
@@ -0,0 +1,9 @@
+import paper from "./paper.png"
+import rock from "./rock.png"
+import scissors from "./scissors.png"
+
+export default {
+    paper,
+    rock,
+    scissors
+}
---
+++ src/components/index.ts
@@ -0,0 +1,11 @@
+import Header from "./Header/Header";
+import Battle from "./Battle/Battle";
+import ChoiceEl from "./Choice/Choice";
+import Footer from "./Footer/Footer";
+
+export {
+    Header,
+    Battle,
+    ChoiceEl as Choice,
+    Footer,
+}
