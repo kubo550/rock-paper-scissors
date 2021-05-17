84b5cd4a08f04afcaa6efa42f83f38526cf95d4b
powerty <qwercy142@gmail.com>
2021-05-17 14:12:44 +0200

    Add Images

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,9 +1,41 @@
+// - Images
 import paper from "./paper.png"
 import rock from "./rock.png"
 import scissors from "./scissors.png"
+import battle from "./battle.svg";
+import reset from "./reset.svg"
 
-export default {
+// ^ Icons
+import iPaper from "./icon-paper.svg";
+import iRock from "./icon-rock.svg";
+import iScissors from "./icon-scissors.svg";
+
+// + Profs
+import player from "./player.svg";
+import computer from "./computer.svg"
+
+export const images = {
     paper,
     rock,
-    scissors
+    scissors,
+    battle,
+    reset
+}
+
+export const icons = {
+    paper: iPaper,
+    rock: iRock,
+    scissors: iScissors,
+}
+
+export const profs = {
+    player,
+    computer,
+}
+
+export const colors = {
+    blue: "#4D63F5",
+    yellow: "#EA9E0F",
+    red: "#D52C4C",
+    gery: "#3B4262"
 }
