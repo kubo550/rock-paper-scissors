f5342b19fafb390607dc84ec622bb3d95d9e0bff
powerty <qwercy142@gmail.com>
2021-05-17 14:15:15 +0200

    Create Footer Comp

---
+++ src/components/Footer/Footer.style.ts
@@ -0,0 +1,20 @@
+import styled from "styled-components";
+
+export const Wrapper = styled.div`
+    width: 100%;
+    border-top: 1px solid grey;
+    display: flex;
+    justify-content: space-evenly;
+    align-items: center;
+    margin-top: 5px;
+    padding-top: 5px;
+`
+
+export const Link = styled.a`
+    text-decoration: none;
+    color: #eee ;
+`
+
+export const Info = styled.p`
+    margin: 0;
+`
--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,5 +1,34 @@
+import * as S from "./Footer.style";
+
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <S.Wrapper>
+      <S.Link href='#!'>Facebook</S.Link>
+
+      <S.Info>
+        Code and design by @
+        <S.Link
+          href='https://github.com/kubo550'
+          title='Jakub Kurdziel'
+          target='_blank'
+        >
+          kubo550
+        </S.Link>{" "}
+        2021®
+      </S.Info>
+
+      <S.Info>
+        Icons from{" "}
+        <S.Link
+          href='https://www.flaticon.com/authors/nikita-golubev'
+          target='_blank'
+          title='Flaticon'
+        >
+          Flaticon
+        </S.Link>
+      </S.Info>
+    </S.Wrapper>
+  );
 };
 
 export default Footer;
