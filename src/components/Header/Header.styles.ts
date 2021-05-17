c9205cdaab133c3e9f60b979037f452ede67d4b5
powerty <qwercy142@gmail.com>
2021-05-17 14:14:56 +0200

    Add reset btn

--- src/components/Header/Header.styles.ts
+++ src/components/Header/Header.styles.ts
@@ -5,8 +5,30 @@ export const Wrapper = styled.div`
     display: flex;
     align-items: center;
     justify-content: space-evenly;
-    
+    color: #eee;
+    position: relative;
+
+
     h3, h1 {
         margin-top: 0;
     }
+    
+    div {
+        display: flex;
+        justify-content:center;
+        align-items:center;
+        
+        img {
+            margin: 0 20px;
+        }
+    }
+
 `;
+
+
+export const Reset = styled.img`
+    position: absolute;
+    top: 0px;
+    right: 20px;
+    cursor: pointer;
+`
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,24 +1,33 @@
 import { useScore } from "../../context/ScoreContext";
+import { profs, images } from "../../assets";
 import * as S from "./Header.styles";
 
 const Header = () => {
   const {
     score: { player, computer },
+    reset,
   } = useScore();
 
   return (
     <S.Wrapper>
       <div>
-        <img src='' alt='' />
+        <img src={profs.player} alt='player' width='40' />
+        <h3>Player</h3>
       </div>
-      <h3>Player</h3>
       <h1>
         {player} : {computer}
       </h1>
-      <h3>Computer</h3>
       <div>
-        <img src='' alt='' />
+        <h3>Comp</h3>
+        <img src={profs.computer} alt='computer' width='40' />
       </div>
+      <S.Reset
+        src={images.reset}
+        alt='Reset button'
+        title='Reset'
+        width='40'
+        onClick={reset}
+      />
     </S.Wrapper>
   );
 };
