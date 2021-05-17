bbc4d7e70725b753a685feb7ec0151f8915fcedf
powerty <qwercy142@gmail.com>
2021-05-17 14:15:31 +0200

    Create Reset fn

--- src/context/ScoreContext.tsx
+++ src/context/ScoreContext.tsx
@@ -8,15 +8,18 @@ type Score = {
 interface ScoreProps {
   score: Score;
   addPoint: (isPointToPlayer: boolean) => void;
+  reset: () => void;
 }
 
 const ScoreContext = createContext({} as ScoreProps);
 
+const initialScore: Score = {
+  player: 0,
+  computer: 0,
+};
+
 export const ScoreProvider: FC = ({ children }) => {
-  const [score, setScore] = useState<Score>({
-    player: 0,
-    computer: 0,
-  });
+  const [score, setScore] = useState<Score>(initialScore);
 
   const addPoint = (toPlayer: boolean) => {
     setScore(prev =>
@@ -26,8 +29,10 @@ export const ScoreProvider: FC = ({ children }) => {
     );
   };
 
+  const reset = () => setScore(initialScore);
+
   return (
-    <ScoreContext.Provider value={{ score, addPoint }}>
+    <ScoreContext.Provider value={{ score, addPoint, reset }}>
       {children}
     </ScoreContext.Provider>
   );
